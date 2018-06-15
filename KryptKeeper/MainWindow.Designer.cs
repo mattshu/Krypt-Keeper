7378021c54e4e734f7112b8d68fd4274a7a6c3e8
mattshu <mattshu67@gmail.com>
2018-06-15 19:26:39 -0400

    Initial commits

---
+++ KryptKeeper/MainWindow.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace KryptKeeper
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listFiles = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listFiles
+            // 
+            this.listFiles.GridLines = true;
+            this.listFiles.Location = new System.Drawing.Point(12, 157);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(513, 151);
+            this.listFiles.TabIndex = 0;
+            this.listFiles.UseCompatibleStateImageBehavior = false;
+            this.listFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 320);
+            this.Controls.Add(this.listFiles);
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Krypt Keeper - File Security";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listFiles;
+    }
+}
+
---
+++ KryptKeeper/MainWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace KryptKeeper
+{
+    public partial class MainWindow : Form
+    {
+        private static ColumnHeader[] GenerateHeader(string[] names)
+        {
+            var headers = new ColumnHeader[names.Length];
+            for (int i = 0; i < headers.Length; i++)
+            {
+                var header = new ColumnHeader {Text = names[i]};
+                headers[i] = header;
+            }
+            return headers;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddFileListHeaders("Name", "Extension", "Size", "Path", "MD5");
+        }
+
+        private void AddFileListHeaders(params string[] names)
+        {
+            var headers = GenerateHeader(names);
+            listFiles.Columns.AddRange(headers);
+        }
+    }
+}
